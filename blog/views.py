2c777e869b332e43c43866664b30697f414a41e0
Your name <you@example.com>
2020-02-13 23:31:55 +0100

    Modification du HTML du site

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    return HttpResponse("""
-        <h1>Bienvenue sur mon blog !</h1>
-        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
-    """)
-
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
